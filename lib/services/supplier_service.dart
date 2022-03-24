769c39ddad3431c760533f1c01dd0309a6fd4d7c
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 16:51:13 +0700

    Create supplier_service.dart

---
+++ lib/services/supplier_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+// import '../models/artikel_model.dart';
+
+class SupplierService {
+  static Future<ArtikelModel> fetchArtikelData() async {
+    final response = await http
+        .get(Uri.parse('https://rizkyn-ujikom.herokuapp.com/api/supplier'));
+
+    if (response.statusCode == 200) {
+      var data = response.body;
+      return SupplierModel.fromJson(jsonDecode(data));
+    } else {
+      throw Exception();
+    }
+  }
+}
