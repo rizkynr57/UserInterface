464f5fa63aad6eebdb086f81f45b9fe9f018d038
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 16:25:16 +0700

    Update data_customer.dart

--- lib/data_customer.dart
+++ lib/data_customer.dart
@@ -1,123 +1,61 @@
 import 'dart:html';
 import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
-class DataCustomer extends StatelessWidget {
+import 'package:flutter/material.dart';
+​
+class Table extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: ListView(
-          // scrollDirection: Axis.horizontal,
           children: [
             Container(
               width: 400,
               height: 200,
               child: Container(
-                child: Row(
-                  children: [
-                    Container(
-                      alignment: Alignment.topLeft,
-                      child: Builder(
-                        builder: (context) => ElevatedButton(
-                            child: Icon(
-                              Icons.arrow_back_rounded,
-                              color: Colors.yellowAccent,
-                              size: 30,
-                            ),
-                            onPressed: () async {
-                              var result = await Navigator.push<String>(
-                                context,
-                                CupertinoPageRoute(
-                                  builder: (context) => HomeScreen(),
-                                ),
-                              );
-                            }),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(left: 100),
-                      child: Text(
-                        "Data Customer",
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    )
-                  ],
-                ),
+                alignment: Alignment.center,
+                child: Text("Data Customer"),
               ),
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                    colors: [Colors.redAccent, Colors.black, Colors.redAccent]),
+                  colors: <Color> [
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                  ],
+                ),
               ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    columns: [
+                      DataColumn(label: Text("No")),
+                      DataColumn(label: Text("Kode")),
+                      DataColumn(label: Text("Nama")),
+                      DataColumn(label: Text("Alamat")),
+                      DataColumn(label: Text("Email")),
+                      DataColumn(label: Text("Telepon")),
+                    ],
+                    rows: [
+                      DataRow(
+                        cells: [
+                          DataCell(Text("1")),
+                          DataCell(Text("CTM0393")),
+                          DataCell(Text("RzQ")),
+                          DataCell(Text("Bandung")),
+                          DataCell(Text("rzq01@gmail.com")),
+                          DataCell(Text("083335435)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
             ),
-            DataTable(
-              columns: [
-                DataColumn(label: Text("No")),
-                DataColumn(label: Text("Kode")),
-                DataColumn(label: Text("Nama")),
-                DataColumn(label: Text("Alamat")),
-                DataColumn(label: Text("Email")),
-                // DataColumn(label: Text("Perusahaan")),
-              ],
-              rows: [
-                DataRow(
-                  cells: [
-                    DataCell(Text("1")),
-                    DataCell(Text("SP08292")),
-                    DataCell(Text("Rzq")),
-                    DataCell(Text("Bandung")),
-                    DataCell(Text("098244242")),
-                    // DataCell(Text("PT Belum Pasti")),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text("2")),
-                    DataCell(Text("SP08292")),
-                    DataCell(Text("Rzq")),
-                    DataCell(Text("Bandung")),
-                    DataCell(Text("098244242")),
-                    // DataCell(Text("PT Belum Pasti")),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text("3")),
-                    DataCell(Text("SP08292")),
-                    DataCell(Text("Rzq")),
-                    DataCell(Text("Bandung")),
-                    DataCell(Text("09824424")),
-                    // DataCell(Text("PT Belum Pasti")),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text("4")),
-                    DataCell(Text("SP08292")),
-                    DataCell(Text("Rzq")),
-                    DataCell(Text("Bandung")),
-                    DataCell(Text("098244242")),
-                    // DataCell(Text("PT Belum Pasti")),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text("5")),
-                    DataCell(Text("SP08292")),
-                    DataCell(Text("Rzq")),
-                    DataCell(Text("Bandung")),
-                    DataCell(Text("098244242")),
-                    // DataCell(Text("PT Belum Pasti")),
-                  ],
-                ),
-              ],
-            )
           ],
         ),
       ),
