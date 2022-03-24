aeaf23a24eb7f8ae1b4371b4769c57bea2f6a137
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 16:54:40 +0700

    Create barangMasuk.service.dart

---
+++ lib/services/barangMasuk.service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+// import '../models/artikel_model.dart';
+
+class BarangMasukService {
+  static Future<BarangMasukModel> fetchArtikelData() async {
+    final response = await http
+        .get(Uri.parse('https://rizkyn-ujikom.herokuapp.com/api/barang-masuk'));
+
+    if (response.statusCode == 200) {
+      var data = response.body;
+      return BarangMasukModel.fromJson(jsonDecode(data));
+    } else {
+      throw Exception();
+    }
+  }
+}
