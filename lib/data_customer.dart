7bdbb68a67533cd8f35beaa9cf05bf6a8fbb2cb2
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 16:27:05 +0700

    Update data_customer.dart

--- lib/data_customer.dart
+++ lib/data_customer.dart
@@ -1,10 +1,9 @@
 import 'dart:html';
 import 'dart:ui';
 import 'home_screen.dart';
-
 import 'package:flutter/material.dart';
 ​
-class Table extends StatelessWidget {
+class DataCustomer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
