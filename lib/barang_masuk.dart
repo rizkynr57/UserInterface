30ce84bd2cfc048cc08fa12983ba1c2ada2def76
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 16:31:18 +0700

    Update barang_masuk.dart

--- lib/barang_masuk.dart
+++ lib/barang_masuk.dart
@@ -1,82 +1,61 @@
 import 'dart:ui';
 import 'home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-
-class BarangMasuk extends StatelessWidget {
+​
+class DataBarangMasuk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: ListView(
-          // scrollDirection: Axis.horizontal,
           children: [
             Container(
-              alignment: Alignment.center,
               width: 400,
               height: 200,
               child: Container(
-                child: Row(
-                  children: [
-                    Container(
-                      alignment: Alignment.topLeft,
-                      child: Builder(
-                        builder: (context) => ElevatedButton(
-                            child: Icon(
-                              Icons.arrow_back_rounded,
-                              color: Colors.yellowAccent,
-                              size: 30,
-                            ),
-                            onPressed: () async {
-                              var result = await Navigator.push<String>(
-                                context,
-                                CupertinoPageRoute(
-                                  builder: (context) => HomeScreen(),
-                                ),
-                              );
-                            }),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(left: 100),
-                      child: Text(
-                        "Data Supplier",
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    )
-                  ],
-                ),
+                alignment: Alignment.center,
+                child: Text("Data Barang Masuk"),
               ),
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                    colors: [Colors.green, Colors.black, Colors.green]),
-              ),
-            ),
-            DataTable(
-              columns: [
-                DataColumn(label: Text("No")),
-                DataColumn(label: Text("Kode")),
-                DataColumn(label: Text("Nama")),
-                DataColumn(label: Text("Alamat")),
-                DataColumn(label: Text("Telepon")),
-                // DataColumn(label: Text("Perusahaan")),
-              ],
-              rows: [
-                DataRow(
-                  cells: [
-                    DataCell(Text("1")),
-                    DataCell(Text("SP08292")),
-                    DataCell(Text("Rzq")),
-                    DataCell(Text("Bandung")),
-                    DataCell(Text("098244242")),
-                    // DataCell(Text("PT Belum Pasti")),
+                  colors: <Color> [
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
                   ],
                 ),
-              ],
-            )
+              ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    columns: [
+                      DataColumn(label: Text("No")),
+                      DataColumn(label: Text("Nama Barang")),
+                      DataColumn(label: Text("Jenis Barang")),
+                      DataColumn(label: Text("Stok Barang")),
+                      DataColumn(label: Text("Harga Beli")),
+                      DataColumn(label: Text("Harga Jual")),
+                      DataColumn(label: Text("Satuan")),
+                    ],
+                    rows: [
+                      DataRow(
+                        cells: [
+                          DataCell(Text("1")),
+                          DataCell(Text("Iphone")),
+                          DataCell(Text("Elektronik")),
+                          DataCell(Text("98")),
+                          DataCell(Text("5.900.000")),
+                          DataCell(Text("6.500.000")),
+                          DataCell(Text("Unit")),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+            ),
           ],
         ),
       ),
