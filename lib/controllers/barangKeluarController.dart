5bb850a4e2bd024a736d3899754aee79e83be520
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-25 17:26:20 +0700

    Update barangKeluarController.dart

---
+++ lib/controllers/barangKeluarController.dart
@@ -0,0 +1,26 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:ujikom/services/barang-keluar_service.dart';
+import '../models/barang-keluar_model.dart';
+
+class BarangKeluarController extends GetxController {
+  var barangKeluarList = RxList<Data>();
+  var isLoading = true.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchBarangKeluarData();
+  }
+
+  void fetchBarangKeluarData() async {
+    try {
+      isLoading(true);
+      var barangKeluar = await BarangKeluarService.fetchBarangKeluarData();
+      barangKeluarList.value = barangKeluar.data as List<Data>;
+      update();
+    } finally {
+      isLoading(false);
+    }
+  }
+}
