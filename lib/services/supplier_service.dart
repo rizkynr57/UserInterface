df79b562cd914ece2e395cd4ed550cd2bfd89f6e
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-25 14:19:36 +0700

    Update supplier_service.dart

--- lib/services/supplier_service.dart
+++ lib/services/supplier_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import '../models/supplier_model.dart';
 
 class SupplierService {
-  static Future<SupplierModel> fetchArtikelData() async {
+  static Future<SupplierModel> fetchSupplierData() async {
     final response = await http
         .get(Uri.parse('https://rizkyn-ujikom.herokuapp.com/api/supplier'));
 
