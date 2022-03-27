7333e6ba4a6f8516d79a0294de9aa61b17a2de0a
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-27 21:10:10 +0700

    Update controller_bindings.dart

--- controller_bindings.dart
+++ controller_bindings.dart
@@ -1,16 +1,1 @@
-import 'package:get/instance_manager.dart';
 
-import 'controllers/supplierController.dart';
-import 'controllers/customerController.dart';
-import 'controllers/barangMasukController.dart';
-import 'controllers/barangKeluarController.dart';
-
-class ControllerBindings extends Bindings {
-  @override
-  void dependencies() {
-    Get.put<SupplierController>(SupplierController());
-    Get.put<CustomerController>(CustomerController());
-    Get.put<BarangMasukController>(BarangMasukController());
-    Get.put<BarangKeluarController>(BarangKeluarController());
-  }
-}
