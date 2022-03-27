b35248eb077799728a843663510a1ed177ee9523
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-27 21:01:05 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreenView(
         navigateRoute: MainScreen(),
