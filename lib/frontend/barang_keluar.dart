5de6e057141db30c91e6012e3543a57913eb543f
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-28 07:15:58 +0700

    Update barang_keluar.dart

--- lib/frontend/barang_keluar.dart
+++ lib/frontend/barang_keluar.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/instance_manager.dart';
 
-import '../controllers/barangKeluarController.dart;
+import '../controllers/barangKeluarController.dart';
 
 class BarangKeluar extends StatelessWidget {
     
