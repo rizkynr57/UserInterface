899d68201270136c7fd33770d36cb219dc9f5f9b
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 18:50:54 +0700

    Update data_customer.dart

--- lib/frontend/data_customer.dart
+++ lib/frontend/data_customer.dart
@@ -1,74 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/instance_manager.dart';
 
 import '../controllers/customerController.dart';
 
 class DataCustomer extends StatelessWidget {
-  
+  const DataCustomer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final CustomerController customerController = Get.put(CustomerController());
-    return MaterialApp(
-      home: Scaffold(
-       appBar: AppBar(
-         leading: IconButton(
+    var size, height, width;
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () => Navigator.of(context).pop(),
-       ),
-        body: ListView(
-          children: [
-            Container(
-              width: 400,
-              height: 200,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text("Data Pelanggan"),
-              ),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: <Color>[
-                    Colors.blue,
-                    Colors.white,
-                    Colors.blue,
-                  ],
-                ),
-              ),
-            ),
-            SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: DataTable(
-                      columns: [
-                        DataColumn(label: Text("No")),
-                        DataColumn(label: Text("Kode")),
-                        DataColumn(label: Text("Nama")),
-                        DataColumn(label: Text("Alamat")),
-                        DataColumn(label: Text("Email")),
-                        DataColumn(label: Text("No. Telepon")),
-                      ],
-                      rows: [
-                        DataRow(
-                          cells: [
-                            DataCell(Text("1")),
-                            DataCell(Text("CTR001")),
-                            DataCell(Text("Rico")),
-                            DataCell(Text("Bandung")),
-                            DataCell(Text("rico@gmail.com")),
-                            DataCell(Text("12312312441")),
-                            // DataCell(Text(customerController.customerList[index].kode.toString())),
-                            // DataCell(Text(customerController.customerList[index].nama.toString())),
-                            // DataCell(Text(customerController.customerList[index].alamat.toString())),
-                            // DataCell(Text(customerController.customerList[index].email.toString())),
-                            // DataCell(Text(customerController.customerList[index].telepon.toString())),
+        ),
+        title: Text("Data Customer"),
+        centerTitle: true,
+      ),
+      body: Obx(
+        () => supplierController.isLoading()
+            ? Center(child: const CircularProgressIndicator())
+            : Center(
+                child: ListView.builder(
+                itemCount: customerController.customerList.length,
+                itemBuilder: (content, index) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        width: 400,
+                        height: 400,
+                        margin: EdgeInsets.only(bottom: 10, top: 10),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Colors.blueAccent, Colors.redAccent]),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Kode : " +
+                                  customerController.customerList[index].kode
+                                      .toString(),
+                              style: TextStyle(backgroundColor: Colors.amber),
+                            ),
+                            Text(
+                                "Nama : " +
+                                    customerController
+                                        .customerList[index].nama
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
+                             Text(
+                                "Alamat : " +
+                                    customerController
+                                        .custometList[index].alamat
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
+                             Text(
+                                "Email: " +
+                                    customerController
+                                        .customerList[index].email
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
+                              Text(
+                                "No Telepon : " +
+                                    customerController
+                                        .customerList[index].telepon
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
                           ],
                         ),
-                      ],
-                    ),
-                 ),
-              ),          
-           ],
-        ),
+                      ),
+                    ],
+                  );
+                },
+              )),
       ),
     );
   }
