06510b0f51111ab6ce3dc1a1818e9c98b94a1182
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 18:21:11 +0700

    Update data_supplier.dart

--- lib/frontend/data_supplier.dart
+++ lib/frontend/data_supplier.dart
@@ -1,76 +1,92 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/instance_manager.dart';
 
 import '../controllers/supplierController.dart';
 
 class DataSupplier extends StatelessWidget {
-  
+  const DataSupplier({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final SupplierController supplierController = Get.put(SupplierController());
-    return MaterialApp(
-      home: Scaffold(
-       appBar: AppBar(
-          leading: IconButton(
+    var size, height, width;
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () => Navigator.of(context).pop(),
         ),
-       ),
-        body: ListView(
-          children: [
-            Container(
-              width: 400,
-              height: 200,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text("Data Leveransir"),
-              ),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: <Color>[
-                    Colors.green,
-                    Colors.white,
-                    Colors.green,
-                  ],
-                ),
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  columns: [
-                    DataColumn(label: Text("No")),
-                    DataColumn(label: Text("Kode")),
-                    DataColumn(label: Text("Nama")),
-                    DataColumn(label: Text("Alamat")),
-                    DataColumn(label: Text("No. Telepon")),
-                    DataColumn(label: Text("Perusahaan")),
-                  ],
-                  rows: [
-                    DataRow(
-                      cells: [
-                        DataCell(Text("1")),
-                        DataCell(Text("SPR001")),
-                        DataCell(Text("Rico")),
-                        DataCell(Text("Bandung")),
-                        DataCell(Text("093213211")),
-                        DataCell(Text("PT")),
-                        // DataCell(Text(supplierController.supplierList[index].kode.toString())),
-                        // DataCell(Text(supplierController.supplierList[index].namaSupplier.toString())),
-                        // DataCell(Text(supplierController.supplierList[index].alamat.toString())),
-                        // DataCell(Text(supplierController.supplierList[index].noTelp.toString())),
-                        // DataCell(Text(supplierController.supplierList[index].namaPerusahaan.toString())),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+        title: Text("Data Supplier"),
+        centerTitle: true,
+      ),
+      body: Obx(
+        () => supplierController.isLoading()
+            ? Center(child: const CircularProgressIndicator())
+            : Center(
+                child: ListView.builder(
+                itemCount: supplierController.supplierList.length,
+                itemBuilder: (content, index) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        width: 400,
+                        height: 400,
+                        margin: EdgeInsets.only(bottom: 10, top: 10),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Colors.blueAccent, Colors.redAccent]),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Kode : " +
+                                  supplierController.supplierList[index].kode
+                                      .toString(),
+                              style: TextStyle(backgroundColor: Colors.amber),
+                            ),
+                            Text(
+                                "Nama : " +
+                                    supplierController
+                                        .suppliefList[index].namaSupplier
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
+                             Text(
+                                "Alamat : " +
+                                    supplierController
+                                        .suppliefList[index].alamat
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
+                             Text(
+                                "No Telepon : " +
+                                    supplierController
+                                        .suppliefList[index].noTelp
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
+                              Text(
+                                "Nama Perusahaan : " +
+                                    supplierController
+                                        .suppliefList[index].namaPerusahaan
+                                        .toString(),
+                                style:
+                                    TextStyle(backgroundColor: Colors.amber)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              )),
       ),
     );
   }
